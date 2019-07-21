54aa3980707787fc9b4c48160976b3caea60a319
zc <zhengcong@souche.com>
2019-07-21 17:40:06 +0800

    升级spring版本

--- swag-web/src/test/java/com/cong/swag/test/BaseTest.java
+++ swag-web/src/test/java/com/cong/swag/test/BaseTest.java
@@ -1,22 +1,6 @@
 package com.cong.swag.test;
 
-import com.cong.swag.common.VO.GoodsVO;
-import com.cong.swag.common.cache.RedisLock;
-import com.cong.swag.common.cache.RedisRepository;
-import com.cong.swag.dao.GoodsDao;
-import com.cong.swag.service.task.factory.JobFactory;
-import com.cong.swag.service.task.factory.TaskProducer;
-import com.cong.swag.service.user.UserService;
-import java.io.IOException;
-import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.quartz.SchedulerException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -30,68 +14,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 @ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
 public class BaseTest {
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    GoodsDao goodsDao;
-
-    @Autowired
-    RedisRepository redisRepository;
-
-    @Autowired
-    RedisLock redisLock;
-
-    @Autowired
-    TaskProducer taskProducer;
-
-    @Autowired
-    JobFactory jobFactory;
-
-    @Test
-    public void goods() {
-        GoodsVO goodsVO = goodsDao.getGoodsById((long) 1);
-        Assert.assertNotNull(goodsVO);
-    }
-
-    @Test
-    public void insertGoods(){
-        GoodsVO vo = new GoodsVO();
-        vo.setStorage(9);
-        int res = goodsDao.addGoods(vo);
-        Assert.assertEquals(1, res);
-    }
-
-    @Test
-    public void testRedis() throws InterruptedException {
-
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        CountDownLatch countDownLatch = new CountDownLatch(5);
-        int i = 0;
-        while (i++<5) {
-            executorService.submit(() -> {
-                while (!redisLock.lock("lock", UUID.randomUUID().toString(), 5)) {
-                    System.out.println(Thread.currentThread().getId()+"并没有获取到锁呀");
-                }
-                System.out.println(Thread.currentThread().getId()+"成功获取到锁");
-                countDownLatch.countDown();
-            });
-        }
-        countDownLatch.await();
-        System.out.println("所有线程都已获取到锁");
-
-    }
-
-    @Test
-    public void testTask() throws IOException, SchedulerException {
-//        JobModel jobModel = new JobModel();
-//        jobModel.setJobCreateDate(new Date());
-//        jobModel.setJobDesc("微博热搜推送");
-//        jobModel.setJobHandler("weiboHotRankPullJobHandler");
-//        taskProducer.addCronJob("weibo_hotrank_push","spider", jobModel, "0 0/15 * * * ?");
-//        System.in.read();
-    }
 
 
 }
